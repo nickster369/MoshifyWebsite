91ef6c53feebfcd37bb1c0945f6b2273dab6df7a
nickster369 <nicolassabbagha802@gmail.com>
2024-05-11 23:23:33 -0300

    uploaded files

---
+++ dist/index.544849a7.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll(".collapsible");e.forEach(e=>e.addEventListener("click",function(){this.classList.toggle("collapsible--expanded")}));
+//# sourceMappingURL=index.544849a7.js.map
